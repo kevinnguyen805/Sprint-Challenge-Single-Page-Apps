6870ced79015775a4e0b8478fba31ef69ebfe2c7
admin <admin@admins-MacBook-Pro.local>
2019-09-20 08:19:20 -0700

    rendered characters onto screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage'
+import CharacterList from './components/CharacterList'
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <CharacterList />
     </main>
   );
 }
+
+
+/*
+
+1. Display Data from a Server API
+
+
+2. Add a Router to this application using React Router
+
+
+3. Add 'Search By Name' feature 
+
+*/
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [characterData, setCharacterData] = useState([])
+
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log(response.data.results)
+        const character = response.data.results
+        setCharacterData(character);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {
+        characterData.map(item => {
+          return (
+            <div key={item.id}>
+              <img src={item.image} alt="character profile" />
+              <h3>{item.name}</h3>
+              <p>{item.species}</p>
+              <p>{item.status}</p>
+            </div>
+          )
+        })
+      }
     </section>
   );
 }
